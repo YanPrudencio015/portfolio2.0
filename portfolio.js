7a3f1e4f84a3a90cc142fab2483fa4aa4b69ee8c
Yan <yanpereira015@gmail.com>
2023-11-24 11:23:14 -0300

    second-Commit
    here I was finished projects container and made Contact container

--- portfolio.js
+++ portfolio.js
@@ -53,14 +53,131 @@ contactBtn.addEventListener("click",()=>{
 })
 
 
+const submenuLinks = document.querySelectorAll(".submenu-profile-links");
+
+submenuLinks.forEach(submenuLink =>{
+    submenuLink.addEventListener("click",()=>{
+        submenuBack.classList.remove("submenu-background-active");
+        contactSubmenu.classList.remove("submenu-contacts-Active")
+        profileSubmenu.classList.remove("submenu-profile-Active")
+    })
+})
+
+
 
 
 }
-
-
-
 menuMobile()
 
 
 
+function MenuSide(){
+const btnOpenMenuSide = document.querySelector(".btn-menu");
+const btnIconOpenMenuSide = document.querySelector(".fa-bars");
+const btnCloseMenuSide = document.querySelector(".menuSide-close-icon");
+const backgroundMenuSide = document.getElementsByClassName("menuSide-Background")[0];
+
+const menuSide = document.querySelector(".menuSide-container");
+
+const optionsBtn1 = document.getElementsByClassName("dropDown-button")[0]
+const optionsBtn2 = document.getElementsByClassName("dropDown-button")[1]
+
+const dropDownContainerProfile = document.getElementsByClassName("dropDown-container")[0]
+const dropDownContainerContact = document.getElementsByClassName("dropDown-container")[1]
+
+document.addEventListener("click", e =>{
+    if(e.target == btnOpenMenuSide || e.target == btnIconOpenMenuSide){
+        backgroundMenuSide.classList.add("backgroundMenuSide-Open");
+        menuSide.classList.add("menuSide-container-Open")
+        
+    }else if(e.target == btnCloseMenuSide || e.target == backgroundMenuSide){
+        backgroundMenuSide.classList.remove("backgroundMenuSide-Open")
+        menuSide.classList.remove("menuSide-container-Open")
+        // backgroundMenuSide.classList.add("menuSide-Background")
+        dropDownContainerContact.classList.remove("dropDown-container-Open")
+        dropDownContainerProfile.classList.remove("dropDown-container-Open")
+
+    }
+    
+    
+    if(e.target == optionsBtn1){
+        console.log("profile")
+        dropDownContainerProfile.classList.toggle("dropDown-container-Open");
+        dropDownContainerContact.classList.remove("dropDown-container-Open")
+    } else if(e.target == optionsBtn2){
+        console.log("contact")
+        dropDownContainerContact.classList.toggle("dropDown-container-Open")
+        dropDownContainerProfile.classList.remove("dropDown-container-Open")
+    } 
+
+})
+
+
+const links = document.getElementsByClassName("dropDown-link")
+
+
+for(var link of links){
+    link.addEventListener("click", ()=>{
+        backgroundMenuSide.classList.add("backgroundMenuSide-Open")
+
+    })
+}
+}
+
+MenuSide()
+
+function CardLogic(){
+
+    const cardsDetailsBtn = document.querySelectorAll(".detailsBtn")
+
+cardsDetailsBtn.forEach( btn=>{
+
+    btn.addEventListener("click",()=>{
+
+        let control =  btn.parentElement;
+        let card = control.parentElement;
+        let details =  card.getElementsByClassName("card-details")[0]
+        
+        details.classList.remove("cardDetailsClose")
+        details.classList.add("cardDetailsOpen");
+
+    })
+})
+
+
+
+const closeDetailsBtns = document.querySelectorAll(".card-detail-title");
+
+closeDetailsBtns.forEach(closeDetailsBtn =>{
+
+    closeDetailsBtn.addEventListener("click", ()=>{
+        const detailsContainer = closeDetailsBtn.closest(".card-details");
+        detailsContainer.classList.add("cardDetailsClose")
+
+
+    })
+})
+
+
+}    
+    
+    CardLogic()
+
+
+
+  
+
+
+
+
+
+
+// const cardCloseBtns = document.querySelectorAll(".card-detail-title")
+// cardCloseBtns.forEach(cardCloseBtn =>{
+//     cardCloseBtn.addEventListener("click", ()=>{
+//         const cardDetailsContainer = cardCloseBtn.closest(".card-details")
+//         cardDetailsContainer.classList.add("cardDetailsClose")
+       
+//     })
+// })
 
